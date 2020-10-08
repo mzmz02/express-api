41d54615285dcb8e21c5b2738901943394b2d128
mzmz__02 <bb.mai.k0702@gmail.com>
2020-10-08 20:30:34 +0900

    create app

---
+++ app/app.js
@@ -0,0 +1,26 @@
+// ライブラリ読み込み
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+
+//body-parserの設定
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000; // port番号を指定
+
+
+// GET http://localhost:3000/api/v1/
+// app.get('/api/v1/',function(req,res){
+//     res.json({
+//         message:"Hello,world"
+//     });
+// });
+
+var router = require('./routes/v1/');
+app.use('/api/v1/', router);
+
+
+//サーバ起動
+app.listen(port);
+console.log('listen on port ' + port);
